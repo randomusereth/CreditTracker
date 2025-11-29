8eee084a1e3d5a160996cd209e3cf6332749ac2a
Henok <henokbelachewstu@gmail.com>
2025-11-29 08:48:07 +0300

    Add language as key to BottomNav to force re-render on language change

--- src/components/navigation/BottomNav.tsx
+++ src/components/navigation/BottomNav.tsx
@@ -47,7 +47,7 @@ export function BottomNav() {
   };
 
   return (
-    <nav className="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 z-50">
+    <nav key={language} className="fixed bottom-0 left-0 right-0 bg-white dark:bg-gray-800 border-t border-gray-200 dark:border-gray-700 z-50">
       <div className="max-w-7xl mx-auto px-4">
         <div className="flex justify-around items-center h-16">
           <Link
